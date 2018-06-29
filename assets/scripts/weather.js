22a43ab7fb13a3d6e514ec4154ca197a8e9eda9d
LefseTime <36971616+LefseTime@users.noreply.github.com>
2018-06-29 14:02:06 -0500

    put weather in functions

--- assets/scripts/weather.js
+++ assets/scripts/weather.js
@@ -1,41 +1,65 @@
-var APIKeyWeather = config.weatherKey
+var APIKeyWeather = config.weatherKey;
 var lat = 44.95;
 var lon = -93.09;
 
 //current conditions
 var queryUrlCurrent = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + APIKeyWeather;
-console.log(queryUrlCurrent);
 
-$.ajax({
-    url: queryUrlCurrent,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-    console.log("Location: "+response.name);
-    console.log("Current Time: "+moment().format("LT"));
-    let forecastTime = moment.unix(response.dt).format("LT");
-    console.log("Weather as of "+forecastTime);
-    console.log("Current Temp: "+response.main.temp+"F");
-    console.log("Humidity: "+response.main.humidity+"%");
-    console.log("Wind Speed: "+response.wind.speed+" mph");
-    console.log("General Conditions: "+response.weather[0].main);
-    console.log("More Detailed Conditions: "+response.weather[0].description);
-});
+let currentTemp;
+
+function getWeather() {
+    $.ajax({
+        url: queryUrlCurrent,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log("Location: " + response.name);
+        console.log("Current Time: " + moment().format("LT"));
+        let forecastTime = moment.unix(response.dt).format("LT");
+        console.log("Weather as of " + forecastTime);
+        console.log("Temp: " + response.main.temp + "F");
+        console.log("Humidity: " + response.main.humidity + "%");
+        console.log("Wind Speed: " + response.wind.speed + " mph");
+        console.log("General Conditions: " + response.weather[0].main);
+        console.log("More Detailed Conditions: " + response.weather[0].description);
+
+        let location = response.name;
+        let currentTime = moment().format("LT");
+        currentTemp = response.main.temp;
+        let humidity = response.main.humidity;
+        let windSpeed = response.wind.speed;
+        let genConditions = response.weather[0].main;
+        let detConditions = response.weather[0].description;
+
+        //update html
+        //make funcitont o get weather and then callback when want it to run (load: update html, stop clicked: send to firebase)
+    });
+};
+getWeather();
+
 
 //future conditions
 var queryUrlFuture = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + APIKeyWeather;
-console.log(queryUrlFuture);
 
-$.ajax({
-    url: queryUrlFuture,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-    let futureTime = moment.unix(response.list[0].dt).format("LT");
-    console.log("Forecast for "+futureTime);
-    console.log("Temp: "+response.list[0].main.temp+"F");
-    console.log("Humidity: "+response.list[0].main.humidity+"%");
-    console.log("Wind Speed: "+response.list[0].wind.speed+" mph");
-    console.log("General Conditions: "+response.list[0].weather[0].main);
-    console.log("More Detailed Conditions: "+response.list[0].weather[0].description);
-});
+function getForecast() {
+    $.ajax({
+        url: queryUrlFuture,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        let futureTime = moment.unix(response.list[0].dt).format("LT");
+        console.log("Forecast for " + futureTime);
+        console.log("Temp: " + response.list[0].main.temp + "F");
+        console.log("Humidity: " + response.list[0].main.humidity + "%");
+        console.log("Wind Speed: " + response.list[0].wind.speed + " mph");
+        console.log("General Conditions: " + response.list[0].weather[0].main);
+        console.log("More Detailed Conditions: " + response.list[0].weather[0].description);
+
+        let futureTemp = response.list[0].main.temp;
+        let futureHumidity = response.list[0].main.humidity;
+        let futureWindSpeed = response.list[0].wind.speed;
+        let futureGenConditions = response.list[0].weather[0].main;
+        let futureDetConditions = response.list[0].weather[0].description;
+    });
+};
+getForecast();
