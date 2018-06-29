21370a003b063f05143355e20ae351284406fd2b
NDKivi <kivineal@outlook.com>
2018-06-29 14:14:55 -0500

    better geoloc testing

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,14 +11,21 @@ $(document).ready(function () {
             let currentTime = coordinates[coordinates.length - 1].time.format("hh:mm:ss.S a");
             $("#geolocation-output").append(`<p>Total distance (meters): ${pathLength}</p>
                                              <p> Time: ${currentTime}</p>
+                                             <p>Data point total: ${coordinates.length}</p>
                                              <p>...</p>`);
             started = false;
+            $(this).text("Stopped... Press to Start");
         } else {
             if (navigator.geolocation) {
+                let counter = 0;
                 watchObject = navigator.geolocation.watchPosition(function (position) {
-                    coordinates.push({ "latitude": position.coords.latitude, "longitude": position.coords.longitude, "time": moment() });
                     started = true;
+                    if ((counter % 20) === 0) {
+                        coordinates.push({ "latitude": position.coords.latitude, "longitude": position.coords.longitude, "time": moment() });
+                    }
+                    counter++;
                 });
+                $(this).text("Running... Press to Stop");
             }
         }
     });
