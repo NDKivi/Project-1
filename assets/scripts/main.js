55c3464a52a6f22297f3e3233d5fbf434d1c119c
NDKivi <kivineal@outlook.com>
2018-07-02 14:45:39 -0500

    update logic in GPS to accomodate start and stop

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,38 +1,18 @@
-// Array of coordinates
+// Array objects: coordinates recorded by browser's watch function
+//                 used to calculate the distance traveled
 let coordinates = [];
+
+// Floating point: number to keep track of precise total distance
+let distanceInMiles = 0.0;
+
+// Object: Coordinates generated on page load so we can get the weather
 let coordinatesOnLoad;
+
+// Object: Needed to stop watching for GPS data once Stop is pressed
 let watchObject;
 
-// Start testing stuff
-// $(document).ready(function () {
-//     $("#get-location").on("click", function () {
-//         if (started) {
-//             navigator.geolocation.clearWatch(watchObject);
-//             let pathLength = geolib.getPathLength(coordinates);
-//             let currentTime = coordinates[coordinates.length - 1].time.format("hh:mm:ss.S a");
-//             $("#geolocation-output").append(`<p>Total distance (meters): ${pathLength}</p>
-//                                              <p> Time: ${currentTime}</p>
-//                                              <p>Data point total: ${coordinates.length}</p>
-//                                              <p>...</p>`);
-//             started = false;
-//             $(this).text("Stopped... Press to Start");
-//         } else {
-//             if (navigator.geolocation) {
-//                 let counter = 0;
-//                 watchObject = navigator.geolocation.watchPosition(function (position) {
-//                     started = true;
-//                     if ((counter % 20) === 0) {
-//                         coordinates.push({ "latitude": position.coords.latitude, "longitude": position.coords.longitude, "time": moment() });
-//                     }
-//                     counter++;
-//                 }, function () {}, {enableHighAccuracy: true});
-//                 $(this).text("Running... Press to Stop");
-//             }
-//         }
-//     });
-// });
-// End testing stuff
-
+/* Prompts the user to allow geolocation, saves current coordinates in coordinatesOnLoad
+and calls getWeather and getForecast to draw the weather info based on the user's coordinates */
 function getCoordinates() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -57,12 +37,18 @@ function startGPS() {
 
 /* Clear the watch function to stop recording coordinates */
 function stopGPS() {
+    distanceInMiles += geolib.getPathLength(coordinates) * 0.00062137;
+    coordinates = [];
     navigator.geolocation.clearWatch(watchObject);
+}
+
+function resetDistance() {
+    distanceInMiles = 0.0;
 }
 
 /* Returns current distance of the path in Miles with two decimal places */
 function getDistance() {
-    return (geolib.getPathLength(coordinates) * 0.00062137).toFixed(2);
+    return distanceInMiles.toFixed(2);
 }
 
 function displayDistance() {
